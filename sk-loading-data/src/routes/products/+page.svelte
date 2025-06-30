b3cd57cc2d0bb6338296d079570ca5d709762eaf
Nimantha Supun <supunnimantha199@gmail.com>
2025-06-30 12:13:58 +0530

    http://localhost:4000/products

---
+++ sk-loading-data/src/routes/products/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let data;
+    const products = data.products
+</script>
+
+<h1>{data.title}</h1>
+
+
+{#each products as product}
+    <div class="mt-10 font-bold">
+        <h1>{product.title}</h1>
+        <h1>{product.description}</h1>
+        <h1>{product.price}</h1>
+    </div>
+{/each}
