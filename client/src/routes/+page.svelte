f8503db2c0f642128f9a2c18f96f9edeb7dfcf12
Nimantha Supun <supunnimantha199@gmail.com>
2025-07-01 19:31:12 +0530

    fix: add missing newline for better readability in +page.svelte

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -15,6 +15,7 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
 <h1 class="text-xl font-bold">Hello</h1>
 
 <a href="/products">product</a><br>
