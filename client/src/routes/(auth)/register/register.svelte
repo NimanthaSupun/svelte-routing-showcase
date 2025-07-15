664062e7874bb16869fbec3ca0ae70575edc1d3d
Nimantha Supun <supunnimantha199@gmail.com>
2025-07-15 19:30:23 +0530

    feat: implement registration form with input fields and message display

--- client/src/routes/(auth)/register/register.svelte
+++ client/src/routes/(auth)/register/register.svelte
@@ -1,2 +1,31 @@
+<script>
+  let name = '';
+  let email = '';
+  let password = '';
+  let message = '';
+
+  function handleRegister() {
+    message = `Registered: ${name} (${email})`;
+  }
+</script>
+
 <h1>Register</h1>
-<div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo distinctio deleniti excepturi.</div>
+<form on:submit|preventDefault={handleRegister} class="space-y-4 max-w-md mx-auto p-4 border rounded">
+  <div>
+    <label for="name" class="block mb-1">Name</label>
+    <input id="name" type="text" bind:value={name} class="border p-2 w-full" required />
+  </div>
+  <div>
+    <label for="email" class="block mb-1">Email</label>
+    <input id="email" type="email" bind:value={email} class="border p-2 w-full" required />
+  </div>
+  <div>
+    <label for="password" class="block mb-1">Password</label>
+    <input id="password" type="password" bind:value={password} class="border p-2 w-full" required />
+  </div>
+  <button type="submit" class="bg-amber-500 text-white px-4 py-2 rounded">Register</button>
+</form>
+
+{#if message}
+  <div class="mt-4 text-green-600">{message}</div>
+{/if}
