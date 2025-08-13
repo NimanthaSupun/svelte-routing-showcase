a2c9afee01d00a182e1bdd4c6afd8d14097f61dc
Nimantha Supun <supunnimantha199@gmail.com>
2025-08-13 14:04:47 +0530

    fix

--- sk-loading-data/src/routes/+page.svelte
+++ sk-loading-data/src/routes/+page.svelte
@@ -33,6 +33,4 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <a href="/products">products</a>
 
-<script>
-    
-</script>
+<!-- Removed duplicate script block -->
